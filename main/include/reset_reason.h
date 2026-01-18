66037197981bc90905ad4dc42d8a94b99f460887
Michael-Kynematik <michael.e.landry@kynematik.com>
2026-01-18 14:12:14 -0600

    BLOCK 6 - unify reset reason mapping

---
+++ main/include/reset_reason.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "esp_system.h"
+
+const char *reset_reason_to_str(esp_reset_reason_t r);
--- main/snapshot.c
+++ main/snapshot.c
@@ -10,6 +10,7 @@
 #include "board.h"
 #include "loadcell_scale.h"
 #include "motor.h"
+#include "reset_reason.h"
 
 #define SNAPSHOT_MAX_FIELDS 16
 
@@ -122,37 +123,6 @@ bool snapshot_append_string(char *buf, size_t len, size_t *used, const char *val
         }
     }
     return snapshot_append_char(buf, len, used, '"');
-}
-
-static const char *reset_reason_to_str(esp_reset_reason_t r)
-{
-    switch (r)
-    {
-    case ESP_RST_UNKNOWN:
-        return "UNKNOWN";
-    case ESP_RST_POWERON:
-        return "POWERON";
-    case ESP_RST_EXT:
-        return "EXT";
-    case ESP_RST_SW:
-        return "SW";
-    case ESP_RST_PANIC:
-        return "PANIC";
-    case ESP_RST_INT_WDT:
-        return "INT_WDT";
-    case ESP_RST_TASK_WDT:
-        return "TASK_WDT";
-    case ESP_RST_WDT:
-        return "WDT";
-    case ESP_RST_DEEPSLEEP:
-        return "DEEPSLEEP";
-    case ESP_RST_BROWNOUT:
-        return "BROWNOUT";
-    case ESP_RST_SDIO:
-        return "SDIO";
-    default:
-        return "OTHER";
-    }
 }
 
 static bool snapshot_field_uptime(char *buf, size_t len, size_t *used)
